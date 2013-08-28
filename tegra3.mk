a6e9ba9f12eb8b8b2b856599fa8422f49f5c94dc
Thomas Wendt <thoemy@gmx.net>
2013-08-29 00:56:01 +0200

    Device repo cleanup part 1
    commit 457e046cf4b652000b1c99d905eff8d1cb0ab250
    Author: Thomas Wendt <thoemy@gmx.net>
    Date:   Wed Aug 28 22:55:16 2013 +0200
    
        Add common asound.conf
    
    commit 1554fcce0716c11c8a5b658a48422402d754198d
    Author: Thomas Wendt <thoemy@gmx.net>
    Date:   Wed Aug 28 21:45:41 2013 +0200
    
        Add common input configs
    
    commit f2fa9a69cae770145e7dd9483ee87dce4ec65849
    Author: Thomas Wendt <thoemy@gmx.net>
    Date:   Wed Aug 28 19:36:48 2013 +0200
    
        Add common defines
    
    commit 5d110cbe7f13dc2ca62698c790ce48b5713007d5
    Author: Thomas Wendt <thoemy@gmx.net>
    Date:   Wed Aug 28 19:35:47 2013 +0200
    
        Add common files
    
    commit 78812f28f33770d5abed1690b26062d68050ce4d
    Author: Thomas Wendt <thoemy@gmx.net>
    Date:   Wed Aug 28 00:34:39 2013 +0200
    
        Add common product packages
    
    commit 7bb214e41d8440905c60cef8fe8c21364a875e9b
    Author: Thomas Wendt <thoemy@gmx.net>
    Date:   Wed Aug 28 00:23:31 2013 +0200
    
        Add common alsa prebuilds
    
    Change-Id: Iec9de1d811ced068823be355351806d068c39199

--- tegra3.mk
+++ tegra3.mk
@@ -13,6 +13,10 @@
 # limitations under the License.
 #
 
+$(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
+
+DEVICE_PACKAGE_OVERLAYS += device/htc/tegra3-common/overlay
+
 # These are the hardware-specific features
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
@@ -32,6 +36,42 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml
+
+# Prebuilt Alsa configs
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/asound.conf:system/etc/asound.conf \
+    $(LOCAL_PATH)/prebuilt/usr/share/alsa/alsa.conf:system/usr/share/alsa/alsa.conf \
+    $(LOCAL_PATH)/prebuilt/usr/share/alsa/cards/aliases.conf:system/usr/share/alsa/cards/aliases.conf \
+    $(LOCAL_PATH)/prebuilt/usr/share/alsa/pcm/center_lfe.conf:system/usr/share/alsa/pcm/center_lfe.conf \
+    $(LOCAL_PATH)/prebuilt/usr/share/alsa/pcm/default.conf:system/usr/share/alsa/pcm/default.conf \
+    $(LOCAL_PATH)/prebuilt/usr/share/alsa/pcm/dmix.conf:system/usr/share/alsa/pcm/dmix.conf \
+    $(LOCAL_PATH)/prebuilt/usr/share/alsa/pcm/dpl.conf:system/usr/share/alsa/pcm/dpl.conf \
+    $(LOCAL_PATH)/prebuilt/usr/share/alsa/pcm/dsnoop.conf:system/usr/share/alsa/pcm/dsnoop.conf \
+    $(LOCAL_PATH)/prebuilt/usr/share/alsa/pcm/front.conf:system/usr/share/alsa/pcm/front.conf \
+    $(LOCAL_PATH)/prebuilt/usr/share/alsa/pcm/iec958.conf:system/usr/share/alsa/pcm/iec958.conf \
+    $(LOCAL_PATH)/prebuilt/usr/share/alsa/pcm/modem.conf:system/usr/share/alsa/pcm/modem.conf \
+    $(LOCAL_PATH)/prebuilt/usr/share/alsa/pcm/rear.conf:system/usr/share/alsa/pcm/rear.conf \
+    $(LOCAL_PATH)/prebuilt/usr/share/alsa/pcm/side.conf:system/usr/share/alsa/pcm/side.conf \
+    $(LOCAL_PATH)/prebuilt/usr/share/alsa/pcm/surround40.conf:system/usr/share/alsa/pcm/surround40.conf \
+    $(LOCAL_PATH)/prebuilt/usr/share/alsa/pcm/surround41.conf:system/usr/share/alsa/pcm/surround41.conf \
+    $(LOCAL_PATH)/prebuilt/usr/share/alsa/pcm/surround50.conf:system/usr/share/alsa/pcm/surround50.conf \
+    $(LOCAL_PATH)/prebuilt/usr/share/alsa/pcm/surround51.conf:system/usr/share/alsa/pcm/surround51.conf \
+    $(LOCAL_PATH)/prebuilt/usr/share/alsa/pcm/surround71.conf:system/usr/share/alsa/pcm/surround71.conf
+
+# Prebuilt Audio/GPS/Camera/Wi-Fi configs
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/nvcamera.conf:system/etc/nvcamera.conf \
+    $(LOCAL_PATH)/configs/enctune.conf:system/etc/enctune.conf
+
+# Input config files
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/prebuilt/usr/idc/atmel-maxtouch.idc:system/usr/idc/atmel-maxtouch.idc \
+    $(LOCAL_PATH)/prebuilt/usr/idc/projector_input.idc:system/usr/idc/projector_input.idc \
+    $(LOCAL_PATH)/prebuilt/usr/idc/synaptics-rmi-touchscreen.idc:system/usr/idc/synaptics-rmi-touchscreen.idc \
+    $(LOCAL_PATH)/prebuilt/usr/idc/tv-touchscreen.idc:system/usr/idc/tv-touchscreen.idc \
+    $(LOCAL_PATH)/prebuilt/usr/keylayout/projector-Keypad.kl:system/usr/keylayout/projector-Keypad.kl \
+    $(LOCAL_PATH)/prebuilt/usr/keylayout/qwerty.kl:system/usr/keylayout/qwerty.kl \
+    $(LOCAL_PATH)/prebuilt/usr/keylayout/tegra-kbc.kl:system/usr/keylayout/tegra-kbc.kl
 
 # Common ramdisk files
 PRODUCT_COPY_FILES += \
@@ -54,6 +94,31 @@ PRODUCT_PACKAGES += \
     hostapd_cli \
     calibrator
 
+# Audio
+PRODUCT_PACKAGES += \
+    audio.a2dp.default \
+    libtinyalsa \
+    libaudioutils
+
+# Filesystem management tools
+PRODUCT_PACKAGES += \
+    make_ext4fs \
+    setup_fs
+
+
+# NFC packages
+PRODUCT_PACKAGES += \
+    libnfc \
+    libnfc_jni \
+    Nfc \
+    Tag \
+    com.android.nfc_extras
+
+# Other apps
+PRODUCT_PACKAGES += \
+    Torch \
+    com.android.future.usb.accessory
+
 # Video
 PRODUCT_PACKAGES += \
     libstagefrighthw
@@ -65,3 +130,7 @@ PRODUCT_PACKAGES += \
 # Configs
 PRODUCT_COPY_FILES += \
     device/htc/tegra3-common/configs/egl.cfg:system/lib/egl/egl.cfg
+
+
+PRODUCT_AAPT_CONFIG := normal hdpi xhdpi
+PRODUCT_AAPT_PREF_CONFIG := xhdpi
